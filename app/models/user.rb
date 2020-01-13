2157c8d0065a35223e1de552e4962562c239dff1
Kathleen Kamali <kathleenkamali@gmail.com>
2020-01-13 13:07:20 -0800

    added omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,19 @@
+require 'pry'
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable
   has_many :summons
   has_many :demons, through: :summons
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.name = auth.info.name
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
 end
